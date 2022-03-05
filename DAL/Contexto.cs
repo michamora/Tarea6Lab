afe156d2f102420c618043edf9346adcdc3b8256
micha mora <michamora4@gmail.com>
2022-03-05 15:42:25 -0400

    Cree el Contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Tarea6Lab.Models;
+
+namespace Tarea6Lab.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Productos> Productos { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> options) : base(options){}
+    }
+}
